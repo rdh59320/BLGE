a9ed5fab4dc12d7bcddd8f22ce496392d99c32d1
Romain D'HAESE / Romain PCR59320 / Roman O'Cry <63501459+rdh59320@users.noreply.github.com>
2024-10-01 11:18:34 +0200

    Update BeLGE-Ubuntu-V2-0.sh
    corrections

--- BeLGE-Ubuntu-V2-0.sh
+++ BeLGE-Ubuntu-V2-0.sh
@@ -11,12 +11,11 @@
 
 # if user is not running the command as root
 
-if [ "$UID" -ne 0 ]
-	then
+if [ "$UID" -ne 0 ] then
 # output message if user has not root priviledges
-	echo "Please run the installer with SUDO!"
+    echo "Please run the installer with SUDO!"
         # stop script         
-        exit
+    exit
 fi
 
 ### Package Installation Request
@@ -29,11 +28,12 @@ echo -e "\n\n   EXPERIMENTAL Package for Steam installation with GE-Proton \n\n"
 # Request for Package installation
 read -p " Type y or Y then press [y/Y] to install or anyother key to quit [DEFAULT] : " repstart
 
-if [ $repstart != "y" -o $repstar != "Y" ] then echo -e "\n\n Package won't be installed \n\n";
-	echo " Good Bye \!\!\!";
-	echo -e "\n\n";
-	sleep 2;
-	exit;
+if [ $repstart != "y" -o $repstar != "Y" ] then 
+    echo -e "\n\n Package won't be installed \n\n";
+    echo " Good Bye \!\!\!";
+    echo -e "\n\n";
+    sleep 2;
+    exit;
 fi;
 
 # Install wget curl, inxi & xz-utils for system infos, package download and extraction
@@ -75,21 +75,23 @@ OS='cat /home/$USER/.BeLGE/Var/OS/OS'
 GPU='cat /home/$USER/.BeLGE/Var/GPU/GPU'
 
 ## Checking OS is compatible 
-if [ $OS = "0" ] then echo -e "Your OS is not supported \n\n Programm must stop now \n\n Package will be deleted from your system \n\n";
-	sleep 1;
-	rm -rf /opt/BeLGE;
-	rm -rf /home/$USER/.BeLGE;
-	sleep 3;
-	exit;
+if [ $OS = "0" ] then 
+    echo -e "Your OS is not supported \n\n Programm must stop now \n\n Package will be deleted from your system \n\n";
+    sleep 1;
+    rm -rf /opt/BeLGE;
+    rm -rf /home/$USER/.BeLGE;
+    sleep 3;
+    exit;
 fi
 
 ## Checking GPU is compatible 
-if [ $GPU = "0" ] then echo -e "Your GPU is not supported \n\n Programm must stop now \n\n Package will be deleted from your system \n\n";
-	sleep 1;
-	rm -rf /opt/BeLGE;
-	rm -rf /home/$USER/.BeLGE;
-	sleep 3;
-	exit;
+if [ $GPU = "0" ] then 
+    echo -e "Your GPU is not supported \n\n Programm must stop now \n\n Package will be deleted from your system \n\n";
+    sleep 1;
+    rm -rf /opt/BeLGE;
+    rm -rf /home/$USER/.BeLGE;
+    sleep 3;
+    exit;
 fi
 
 ## Confirmation request of the package installation
@@ -97,12 +99,13 @@ fi
 echo -e "\n\n\n  Your system is suitable for installation \n\n\n Would you like to install the BeLGE package ? \n Type n or N then press [ENTER] to abort installation \n"
 read -p " Or press Any Other Key to Continue [DEFAULT] : " repinstall
 
-if [ $repinstall = "n" -o $repinstall = "N" ] then echo -e "\n\n Installation has been aborted \n\n Program will shutdown now and \n\n Package will be deleted from your system \n\n";
-	sleep 1;
-	rm -rf /opt/BeLGE;
-	rm -rf /home/$USER/.BeLGE
-	sleep 3;
-	exit;
+if [ $repinstall = "n" -o $repinstall = "N" ] then 
+    echo -e "\n\n Installation has been aborted \n\n Program will shutdown now and \n\n Package will be deleted from your system \n\n";
+    sleep 1;
+    rm -rf /opt/BeLGE;
+    rm -rf /home/$USER/.BeLGE
+    sleep 3;
+    exit;
 fi
 
 # System configuration script
