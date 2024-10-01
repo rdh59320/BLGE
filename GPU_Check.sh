b2ceac289665d37bcfdb9fca5e61d24f9989aaa5
Romain D'HAESE / Romain PCR59320 / Roman O'Cry <63501459+rdh59320@users.noreply.github.com>
2024-10-01 12:42:31 +0200

    Update GPU_Check.sh
    updating corrections

--- GPU_Check.sh
+++ GPU_Check.sh
@@ -1,56 +1,22 @@
 #!/bin/bash
 
 # Designer : Roman PCR59320 aka Roman O'Cry  (pcr59320@gmail.com / https://github.com/rdh59320/)
-# Description : GPU supplier check script
+# Description : WineHQ staging installation script
 # Licence CREATIVE COMMONS CC BY-NC-SA 4.0
 # Version : 1.0
 
-# Begin GPU_Check script
+# Begin script
 
-# Creating local installation variables files in User directory
-mkdir /home/$USER/.BeLGE/Var/GPU
+GPU_INFO=$(inxi -G | grep "Device-1")
 
-# Condition GPU is neither amd, intel nor nvidia=> GPU variable is set to 0
-echo "0" > /home/$USER/.BeLGE/Var/GPU/GPU;
+if echo "$GPU_INFO" | grep -qi "AMD"; then
+    echo "AMD" > /home/$USER/.BeLGE/GPU
+elif echo "$GPU_INFO" | grep -qi "Intel"; then
+    echo "Intel" > /home/$USER/.BeLGE/GPU
+elif echo "$GPU_INFO" | grep -qi "NVIDIA"; then
+    echo "NVIDIA" > /home/$USER/.BeLGE/GPU
+else
+    echo "0" > /home/$USER/.BeLGE/GPU
+fi
 
-# Graphics used device (Device-1) request from inxi function (file will be empty if request of the main device is not affected to the target supplier)
-
-inxi -G | grep "Device-1" | grep "AMD" > /home/$USER/.BeLGE/Var/GPU/AMD
-inxi -G | grep "Device-1" | grep "Intel" > /home/$USER/.BeLGE/Var/GPU/Intel
-inxi -G | grep "Device-1" | grep "NVIDIA" > /home/$USER/.BeLGE/Var/GPU/NVIDIA
-
-amd='cat /home/$USER/.BeLGE/Var/GPU/AMD'
-intel='cat /home/$USER/.BeLGE/Var/GPU/Intel'
-nvidia='cat /home/$USER/.BeLGE/Var/GPU/NVIDIA'
-
-# Variables simplification into boolean (empty=0, non-empty=1)
-
-# Case AMD file is tested empty then AMD is false and boolean var is switched to 0 (else 1 if Device-1 is AMD)
-if [ ! -s "$amd" ] then 
-    echo "0" > /home/$USER/.BeLGE/Var/GPU/AMD;
-
-else 
-    echo "amd" > /home/$USER/.BeLGE/Var/GPU/GPU;
-	
-fi;
-
-# Case Intel file is tested empty then AMD is false and boolean var is switched to 0 (else 1 if Device-1 is AMD)
-if [ ! -s "$intel" ] then 
-    echo "0" > /home/$USER/.BeLGE/Var/GPU/Intel;
-
-else 
-    echo "intel" > /home/$USER/.BeLGE/Var/GPU/GPU;
-	
-fi;
-	
-# Case NVIDIA file is tested empty then AMD is false and boolean var is switched to 0 (else 1 if Device-1 is AMD)	
-if [ ! -s "$nvidia" ] then 
-    echo "0" > /home/$USER/.BeLGE/Var/GPU/NVIDIA;
-
-else 
-    echo "nvidia" > /home/$USER/.BeLGE/Var/GPU/GPU;
-	
-fi;
-
-# Back to main script
-# End of the script
+# End script
