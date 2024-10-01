2056f7a2d739c104d52c395b1aec9667e39ea64a
Romain D'HAESE / Romain PCR59320 / Roman O'Cry <63501459+rdh59320@users.noreply.github.com>
2024-10-01 11:20:03 +0200

    Update OS_Check.sh
    corrections update

--- OS_Check.sh
+++ OS_Check.sh
@@ -30,27 +30,33 @@ NN='cat /home/$USER/.BeLGE/Var/OS/NN'
 # If one of the supported distros is detected then one of the 3 var files below won't be equal to 0 but 1 
 
 # Case test when /opt/BeLGE/Var/OS/FF file is empty
-if [ ! -s "$FF" ] then echo "0" > /home/$USER/.BeLGE/Var/OS/FF;
+if [ ! -s "$FF" ] then 
+    echo "0" > /home/$USER/.BeLGE/Var/OS/FF;
 
 	# Case test when non empty (focal fossa detected)
-else echo echo "focal" > /home/$USER/.BeLGE/Var/OS/OS;
+else 
+    echo echo "focal" > /home/$USER/.BeLGE/Var/OS/OS;
 
 fi;
 
 # Case test when /opt/BeLGE/Var/OS/JJ file is empty
-if [ ! -s "$JJ" ] then echo "0" > /home/$USER/.BeLGE/Var/OS/JJ;
+if [ ! -s "$JJ" ] then
+    echo "0" > /home/$USER/.BeLGE/Var/OS/JJ;
 
 	# Case test when non empty (jammy jellyfish detected)
-else echo "jammy" > /home/$USER/.BeLGE/Var/OS/OS;
+else 
+    echo "jammy" > /home/$USER/.BeLGE/Var/OS/OS;
 	# OS variable would be needed to determine the right config when file not empty #(distro has been detected) 
 
 fi;
 
 # Case test when /opt/BeLGE/Var/OS/NN file is empty
-if [ ! -s "$JJ" ] then echo "0" > /home/$USER/.BeLGE/Var/OS/NN;
+if [ ! -s "$JJ" ] then
+    echo "0" > /home/$USER/.BeLGE/Var/OS/NN;
 
 	# Case test when non empty (noble numbat detected)
-else echo "noble" > /home/$USER/.BeLGE/Var/OS/OS;
+else
+    echo "noble" > /home/$USER/.BeLGE/Var/OS/OS;
 	# OS variable would be needed to determine the right config when file not empty #(distro has been detected) 
 	
 fi;
