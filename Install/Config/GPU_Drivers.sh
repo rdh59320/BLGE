b073cb96eee79f9543355de5c19c39c85d6e9fda
Romain D'HAESE / Romain PCR59320 / Roman O'Cry <63501459+rdh59320@users.noreply.github.com>
2024-10-09 14:08:09 +0200

    Create GPU_Drivers.sh
    new version

---
+++ Install/Config/GPU_Drivers.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Designer : Roman PCR59320 aka Roman O'Cry  (pcr59320@gmail.com / https://github.com/rdh59320/)
+# Description : GPU Drivers script
+# Licence CREATIVE COMMONS CC BY-NC-SA 4.0
+# Version : 1.1
+
+set -e  # Exit immediately if a command exits with a non-zero status.
+
+# Function for error handling
+handle_error() {
+    echo "Error occurred in script at line: $1"
+    exit 1
+}
+
+trap 'handle_error $LINENO' ERR
+
+# Begin script
+
+## Getting the GPU supplier variable
+GPU=$(cat "/home/$USER/.BeLGE/GPU")
+
+## Applying the right PPA according to the detected GPU
+
+# Case where AMD or Intel is the main GPU supplier
+if [ "$GPU" = "AMD" ] || [ "$GPU" = "Intel" ]; then
+    echo -e "\n\n\n Adding Intel/AMD Mesa/Vulkan Drivers\n\n\n"
+    sudo add-apt-repository ppa:kisak/kisak-mesa -y
+    sudo apt update
+    sudo apt install -y libgl1-mesa-dri:i386 mesa-vulkan-drivers mesa-vulkan-drivers:i386
+    
+# Case where NVIDIA is the main GPU supplier
+elif [ "$GPU" = "NVIDIA" ]; then
+    echo -e "\n\n\n Adding NVIDIA Drivers \n\n\n"
+    sudo add-apt-repository ppa:graphics-drivers/ppa -y
+    sudo apt update
+    sudo apt install -y nvidia-driver-550 libvulkan1 libvulkan1:i386
+else
+    echo "Unsupported GPU type: $GPU"
+    exit 1
+fi
+
+echo "GPU drivers installation completed successfully."
+
+# End script
